d1f7882e618b96085986f7f20b7a92105b2adc29
muuli2 <supissara.chitphitak@gmail.com>
2025-11-25 04:05:33 +0700

    up

---
+++ RePurifyTheRecycalia/Assets/Scripts/FadePanel.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1f;
+    public float holdTime = 1f;   // ⭐ เพิ่ม: ช่วงค้างตอนดำสนิท
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeToScene(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+        while (t > 0f)
+        {
+            t -= Time.deltaTime / fadeDuration;
+            fadeImage.color = new Color(0, 0, 0, t);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / fadeDuration;
+            fadeImage.color = new Color(0, 0, 0, t);
+            yield return null;
+        }
+
+        // ⭐ ค้างตอนดำสนิท
+        yield return new WaitForSeconds(holdTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
