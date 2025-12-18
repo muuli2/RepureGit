35265fb6d4efa8aee89a6b9df17cf3d218c052e2
muuli2 <supissara.chitphitak@gmail.com>
2025-12-18 23:51:53 +0700

    fdsf

--- RePurifyTheRecycalia/Assets/Scripts/Minigame02/RhythmGame2.cs
+++ RePurifyTheRecycalia/Assets/Scripts/Minigame02/RhythmGame2.cs
@@ -385,7 +385,7 @@ if (player != null)
 }
 
     // Unload scene แล้วเรียกบอส
-    SceneManager.UnloadSceneAsync("MiniGame02").completed += (op) =>
+    SceneManager.UnloadSceneAsync("MiniGame022").completed += (op) =>
     {
 
         
